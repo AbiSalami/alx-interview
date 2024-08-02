e34ac3fee2bd7b4b3bd2388f69501707b005eb99
AbiSalami <bimwealth215@gmail.com>
2024-08-02 01:16:31 +0000

    task0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
 """ Module that checks locked boxes """
 
-
 def canUnlockAll(boxes):
     """ Method that determines if all boxes can be opened """
-
-    if boxes == 0:
-        return False
-
     if not isinstance(boxes, list):
+        return False
 
     if len(boxes) == 0:
         return False
 
-    check = [0]
-    list_ing = [i for i in range(len(boxes))]
-    for in_check in check:
-        for in_boxes in boxes[in_check]:
-            if in_boxes not in check and in_boxes in list_ing:
-                if in_boxes >= len(boxes):
-                    return False
-                check.append(in_boxes)
+    opened = [0]
+    total_boxes = len(boxes)
+    list_ing = list(range(total_boxes))
 
-                if len(check) == len(boxes):
-        return True
-    else:
-        return False
+    for in_check in opened:
+        for key in boxes[in_check]:
+            if key not in opened and key in list_ing:
+                if key >= total_boxes:
+                    return False
+                opened.append(key)
+                if len(opened) == total_boxes:
+                    return True
+    return len(opened) == total_boxes
+
