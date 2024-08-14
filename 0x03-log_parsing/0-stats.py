223a37a2491ffa6ea24371f7d7ea78c251e3467b
AbiSalami <bimwealth215@gmail.com>
2024-08-14 20:47:09 +0000

    task0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,59 @@
 #!/usr/bin/python3
+
 import sys
-import signal
+
+def print_msg(dict_sc, total_file_size):
+    """
+    Method to print the statistics.
+    
+    Args:
+        dict_sc: dict of status codes and their counts
+        total_file_size: total file size computed
+    Returns:
+        Nothing
+    """
+    print("File size: {}".format(total_file_size))
+    for key in sorted(dict_sc):
+        if dict_sc[key] > 0:
+            print("{}: {}".format(key, dict_sc[key]))
 
 total_file_size = 0
-status_codes_count = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
+dict_sc = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
 }
-line_count = 0
-
-def print_stats():
-    print("File size: {}".format(total_file_size))
-    for code in sorted(status_codes_count.keys()):
-        if status_codes_count[code] > 0:
-            print("{}: {}".format(code, status_codes_count[code]))
-
-def signal_handler(sig, frame):
-    print_stats()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, signal_handler)
+counter = 0
 
 try:
     for line in sys.stdin:
         parts = line.split()
+        
         if len(parts) < 7:
             continue
-        status_code = parts[-2]
+        
         try:
             file_size = int(parts[-1])
-            total_file_size += file_size
+            status_code = parts[-2]
         except ValueError:
             continue
 
-        if status_code in status_codes_count:
-            status_codes_count[status_code] += 1
-
-        line_count += 1
-        if line_count == 10:
-            print_stats()
-            line_count = 0
+        if status_code in dict_sc:
+            total_file_size += file_size
+            dict_sc[status_code] += 1
+            counter += 1
+        
+        if counter == 10:
+            print_msg(dict_sc, total_file_size)
+            counter = 0
+            # Reset status code counts to start fresh for the next 10 lines
+            dict_sc = {k: 0 for k in dict_sc}
 
 finally:
-    print_stats()
+    print_msg(dict_sc, total_file_size)
 
