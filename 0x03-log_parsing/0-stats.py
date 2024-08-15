43f3bb0265d3735c479c0f1923556ecb0bcd9348
AbiSalami <bimwealth215@gmail.com>
2024-08-15 23:45:59 +0000

    task0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,59 +1,51 @@
 #!/usr/bin/python3
+"""
+log parsing
+"""
 
 import sys
+import re
 
-def print_msg(dict_sc, total_file_size):
+
+def output(log: dict) -> None:
     """
-    Method to print the statistics.
-    
-    Args:
-        dict_sc: dict of status codes and their counts
-        total_file_size: total file size computed
-    Returns:
-        Nothing
+    helper function to display stats
     """
-    print("File size: {}".format(total_file_size))
-    for key in sorted(dict_sc):
-        if dict_sc[key] > 0:
-            print("{}: {}".format(key, dict_sc[key]))
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
-total_file_size = 0
-dict_sc = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-counter = 0
 
-try:
-    for line in sys.stdin:
-        parts = line.split()
-        
-        if len(parts) < 7:
-            continue
-        
-        try:
-            file_size = int(parts[-1])
-            status_code = parts[-2]
-        except ValueError:
-            continue
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
-        if status_code in dict_sc:
-            total_file_size += file_size
-            dict_sc[status_code] += 1
-            counter += 1
-        
-        if counter == 10:
-            print_msg(dict_sc, total_file_size)
-            counter = 0
-            # Reset status code counts to start fresh for the next 10 lines
-            dict_sc = {k: 0 for k in dict_sc}
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
-finally:
-    print_msg(dict_sc, total_file_size)
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
 
