7b5025e5ee3c3b07aa60ba7125692c4e30fb8573
AbiSalami <bimwealth215@gmail.com>
2024-09-05 15:51:38 -0700

    task0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,21 +3,27 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(url, async (err, res, body) => {
-  err && console.log(err);
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-  const charactersArray = (JSON.parse(res.body).characters);
+  const charactersArray = JSON.parse(body).characters;
   for (const character of charactersArray) {
-    await new Promise((resolve, reject) => {
+    await new Promise((resolve) => {
       request(character, (err, res, body) => {
-        err && console.log(err);
-
+        if (err) {
+          console.log(err);
+          resolve();
+          return;
+        }
         console.log(JSON.parse(body).name);
         resolve();
       });
     });
   }
 });
+;
